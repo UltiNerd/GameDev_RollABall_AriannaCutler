d548045ca8976716eac0dc5cc9a6139e9c4bda89
UltiNerd <arianna.eunice@gmail.com>
2025-11-03 15:32:11 -0400

    Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public float jumpstrength = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     private Rigidbody rb;
@@ -12,6 +14,7 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public GameObject LoseTextObject;
+    public bool Jumped;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,7 +28,7 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue movementValue)
     {
-       Vector2 movementVector = movementValue.Get<Vector2>();
+        Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
@@ -43,17 +46,32 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+    }
+    private void Update()
+    {
+        if (Jumped == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                Vector3 jump = new Vector3(0, 1, 0);
+                rb.AddForce(jump * jumpstrength);
+                Jumped = true;
+
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
-          other.gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
         }
-       
+
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -63,7 +81,23 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
             // Update the winText to display "You Lose!"
             LoseTextObject.gameObject.SetActive(true);
-           
+
+
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Jumped = false;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Jumped = true;
         }
     }
 }
