ae070b3bf490b606ac388f9bb5fb16afb970dd57
UltiNerd <arianna.eunice@gmail.com>
2025-11-05 10:37:21 -0400

    Working on Camera Script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,57 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    //video at 3 minutes
+    [SerializeField] private Transform target;
     private Vector3 offset;
+    [SerializeField] private float smoothtime;
+    private Vector3 _currentVelocity = Vector3.zero;
+
+    private Vector2 _input;
+
+    [SerializeField] private MouseSensitivity mouseSensitivity;
+    private CameraRotation cameraRotation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        offset = transform.position - player.transform.position; 
+        offset = transform.position - target.transform.position;
 
     }
 
+    public void Look(InputAction.CallbackContext context) 
+    {
+        _input = context.ReadValue<Vector2>();
+
+    }
+    private void Update()
+    {
+        cameraRotation.Yaw += _input.x * mouseSensitivity.horizontal * Time.deltaTime;
+    }
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        var targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothtime);
 
     }
+}
+
+[Serializable]
+public struct MouseSensitivity 
+{
+    public float horizontal;
+    public float vertical;
+
+}
+
+public struct CameraRotation
+{
+    public float Pitch;
+    public float Yaw;
+
 }
